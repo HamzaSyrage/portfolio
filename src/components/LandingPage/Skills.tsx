cc6fc5c95cd50b105576d3ba08998d473c8e0f1f
HamzaSyrage <hamzasyrage@gmail.com>
2024-10-11 23:50:39 +0300

    edit skills

--- src/components/LandingPage/Skills.tsx
+++ src/components/LandingPage/Skills.tsx
@@ -6,17 +6,16 @@ export default function Skills() {
         <li>REACT</li>
         <li>NEXT</li>
         <li>React Router</li>
-        <li>REACT QUERY</li>
+        <li>TANSTACK QUERY</li>
         <li>Redux & Redux-toolkit</li>
         <li>framer-motion</li>
+        <li>RESTful APIs</li>
         <li>JavaScript (ES6+)</li>
         <li>TypeScript</li>
         <li>HTML5</li>
         <li>CSS3</li>
         <li>Tailwind & BootStrap</li>
         <li>Git & GitHub</li>
-        {/* <li></li> */}
-        {/* spel check those */}
       </ul>
     </section>
   );
